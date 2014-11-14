9a2a407f546309db42ec0cea970c882070d1c190
Laura Dragan <aprilush@gmail.com>
2014-11-14 15:36:06 +0000

    here we go

--- node/client/js/trends.js
+++ node/client/js/trends.js
@@ -4,36 +4,38 @@ angular.module('trends', ['btford.socket-io'])
         var myIoSocket = io.connect('http://localhost:3000/'),
             socket = socketFactory({ ioSocket: myIoSocket });
         return socket;
-    }).controller('main', function($scope, mysocket, $sce, $http) { 
+    }).controller('main', function($scope, mysocket, $sce, $http, $q) { 
         mysocket.addListener("trends_hose", function (data) {
             console.log("trends", data);
             data = JSON.parse(data.data);
         });
 
         function loadMeta() {
-            
+
             $.getJSON("contents.json").success(function(json) {
-                console.log(json); 
+                // console.log(json); 
                 $scope.locations = json.locations;
-                $scope.socmacs = [];
+                $scope.socmacs = json.sources;
                 for (source in json.sources) {
-                    $scope.socmacs[source] = json.sources[source];
                     for (loc in json.sources[source].locations) {
                         $scope.socmacs[source].locations[loc] = $scope.locations[json.sources[source].locations[loc]];
                     }
                 }
+                console.log($scope.socmacs);
             });
         }
 
         function loadData() {
             $.getJSON("trends.json").success(function(json) {
-                console.log(json);
-
+                // console.log(json);
+                for (tti in json) {
+                    tt = json[tti];
+                    $scope.socmacs[tt.source].locations[tt.location]['trends'] = tt.trends;
+                }
             });
         }
 
         loadMeta();
-        console.log($scope.socmacs);       
         loadData();
 
     });
